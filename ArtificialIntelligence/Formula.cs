83364bc076757b8ade60c6f3fc5025ecea6642fd
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-12-29 12:29:39 +0200

    Add project files.

---
+++ ArtificialIntelligence/Formula.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArtificialIntelligence
+{
+    public abstract class Formula
+    {
+        public abstract bool Evaluate();
+        public abstract IEnumerable<Variable> Variables();
+    }
+}
---
+++ ArtificialIntelligence/Variable.cs
@@ -0,0 +1,6 @@
+﻿namespace ArtificialIntelligence
+{
+    public class Variable
+    {
+    }
+}
